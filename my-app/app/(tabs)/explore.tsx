521bfee1148443afb24c1272001d1fbe04d9c613
leung-oscar <leung.oscar@dabba-eats.com>
2025-01-06 23:35:32 +0000

    update color and msg

--- my-app/app/(tabs)/explore.tsx
+++ my-app/app/(tabs)/explore.tsx
@@ -6,11 +6,12 @@ import ParallaxScrollView from '@/components/ParallaxScrollView';
 import { ThemedText } from '@/components/ThemedText';
 import { ThemedView } from '@/components/ThemedView';
 import { IconSymbol } from '@/components/ui/IconSymbol';
+import React from 'react';
 
 export default function TabTwoScreen() {
   return (
     <ParallaxScrollView
-      headerBackgroundColor={{ light: '#D0D0D0', dark: '#353636' }}
+      headerBackgroundColor={{ light: '#D0D0D0', dark: '#FFFF00' }}
       headerImage={
         <IconSymbol
           size={310}
