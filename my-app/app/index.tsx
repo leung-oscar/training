96325ee7bb57953cf388634ec30ec08eb36f16df
leung-oscar <leung.oscar@dabba-eats.com>
2025-01-08 22:17:45 +0000

    Add Restaurant Items

--- my-app/app/index.tsx
+++ my-app/app/index.tsx
@@ -1,19 +1,27 @@
 import React from "react";
 import { Text, View, Image, StyleSheet } from "react-native";
 
-export default function Index() {
+const RestaurantItem = () => {
   return (
-    <View style={styles.container}>
-      {/* Text */}
-      <Text style={styles.header}>Welcome to our menu!</Text> 
-      {/* Restaurant Item */}
-      
-        <Image style={styles.image}
-          source={{ uri: "https://notjustdev-dummy.s3.us-east-2.amazonaws.com/uber-eats/restaurant1.jpeg" }}
+    <View>
+        <Image 
+          style={styles.image}
+          source={{ uri: "https://notjustdev-dummy.s3.us-east-2.amazonaws.com/uber-eats/restaurant1.jpeg"}}
           />
           <Text style={styles.title}>The Bomb</Text>
-          
           <Text style={styles.subtitles}>$14.99 ~10-15 minutes</Text>
+    </View>
+  );
+};
+    
+export default function Index() {
+      return (
+        <View style={styles.container}>
+        {/* Text */}
+        <Text style={styles.header}>Welcome to our menu!</Text> 
+        {/* Restaurant Item */}
+        <RestaurantItem/>
+        <RestaurantItem/>
       </View>
     
   );
