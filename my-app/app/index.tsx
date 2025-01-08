b1eb3af7395d424ba14ffbd0c0eca7260405124d
leung-oscar <leung.oscar@dabba-eats.com>
2025-01-08 19:40:40 +0000

    Learned styles and aspect ratio

--- my-app/app/index.tsx
+++ my-app/app/index.tsx
@@ -1,23 +1,34 @@
 import React from "react";
-import { Text, View, Image } from "react-native";
+import { Text, View, Image, StyleSheet } from "react-native";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-
-    <Text>Hello World!</Text> 
+    <View style={styles.container}>
+      {/* Text */}
+      <Text>Hello World!</Text> 
+      {/* Restaurant Item */}
+      
+        <Image style={styles.image}
+          source={{ uri: "https://notjustdev-dummy.s3.us-east-2.amazonaws.com/uber-eats/restaurant1.jpeg" }}
+        />
+      </View>
     
-    <Image 
-      source={{ uri: "https://notjustdev-dummy.s3.us-east-2.amazonaws.com/uber-eats/restaurant1.jpeg" }}
-      style={{ width: 200, height: 200 }} // You might want to specify width and height
-    />
-
-    </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container:{
+    flex: 1,
+    backgroundColor: "lightgreen",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  restraurantContainer:{
+    height: "100%",
+    width: 300,
+  },
+  image:{
+    height: "100%",
+    aspectRatio: 5/3,
+  }
+})
