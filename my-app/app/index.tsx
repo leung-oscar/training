5a356627d6ae235b98155be091efb4e3fcb27b31
leung-oscar <leung.oscar@dabba-eats.com>
2025-01-09 23:27:06 +0000

    add styles and lightgreen background

--- my-app/app/index.tsx
+++ my-app/app/index.tsx
@@ -1,16 +1,19 @@
 import React from "react";
-import { Text, View } from "react-native";
+import { Text, View, StyleSheet} from "react-native";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
+    <View style={styles.container}>
       <Text>Hello Oscar Worlds!</Text>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex:1,
+    backgroundColor: "lightblue",
+    alignItems:"center",
+    justifyContent:"center"
+  }
+})
