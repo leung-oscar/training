4819ae8bf9488a9a8eeb77731d826cc1cdd63192
leung-oscar <leung.oscar@dabba-eats.com>
2025-01-10 08:17:03 +0000

    Add flatlist to dishes

--- my-app/app/screens/restaurantDetails.tsx
+++ my-app/app/screens/restaurantDetails.tsx
@@ -3,6 +3,7 @@ import {View, Text, Image, FlatList, StyleSheet} from 'react-native';
 import restaurants from '/workspaces/training/my-app/assets/data/restaurants.json'
 import { Ionicons } from '@expo/vector-icons';
 import DishListItems from '/workspaces/training/my-app/assets/components/dishListItem.jsx'
+import DishListItem from '/workspaces/training/my-app/assets/components/dishListItem.jsx';
 
 const restaurant = restaurants[0];
 
@@ -23,10 +24,11 @@ const RestaurantDetailsPage = () => {
                 <Text style={styles.subtitle}>Delivery Fee {restaurant.deliveryFee}</Text>
                 <Text style={styles.subtitle}>Delivery Time ~{restaurant.minDeliveryTime}-{restaurant.maxDeliveryTime}</Text>
             </View>
-
-            <DishListItems dish={restaurant.dishes[0]} />
-            <DishListItems dish={restaurant.dishes[1]} />
-            <DishListItems dish={restaurant.dishes[2]} />
+            
+            <FlatList
+                data={restaurant.dishes}
+                renderItem={({item})=> <DishListItem dish={item}/> }
+            />
 
 
         </View>
