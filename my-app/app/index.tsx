b4c116a5adc58d11844806626675bf0de1a28694
leung-oscar <leung.oscar@dabba-eats.com>
2025-01-10 02:01:49 +0000

    Add flatlist rendering

--- my-app/app/index.tsx
+++ my-app/app/index.tsx
@@ -1,16 +1,20 @@
 import React from "react";
-import { Text, View, StyleSheet, Image} from "react-native";
+import {FlatList, View, StyleSheet} from "react-native";
 import RestaurantItem from "/workspaces/training/my-app/assets/components/restaurantItem.jsx"
 import restaurant from "/workspaces/training/my-app/assets/data/restaurants.json"
 export default function Index() {
   return (
     <View style={styles.container}>
       {/* Restaurant Item */}
-        <RestaurantItem restaurant = {restaurant[0]}/>
+        {/* <RestaurantItem restaurant = {restaurant[0]}/>
         <RestaurantItem restaurant = {restaurant[1]}/>
         <RestaurantItem restaurant = {restaurant[2]}/>
-        <RestaurantItem restaurant = {restaurant[3]}/>
+        <RestaurantItem restaurant = {restaurant[3]}/> */}
 
+      <FlatList
+        data={restaurant}
+        renderItem={({item}) => <RestaurantItem restaurant={item}/>}
+      />
     </View>
   );
 }
