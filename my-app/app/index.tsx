7b62d6053a1e47513db1ea24dca204c69a5cef11
leung-oscar <leung.oscar@dabba-eats.com>
2025-01-13 23:04:38 +0000

    Added DishDetailsScreen

--- my-app/app/index.tsx
+++ my-app/app/index.tsx
@@ -2,11 +2,13 @@ import React from "react";
 import {View, StyleSheet} from "react-native";
 import HomeScreen from "./screens/home";
 import RestaurantDetailsPage from "./screens/ResaurantDetailsScreen";
+import DishDetailsScreen from "./screens/DishDetailsScreen";
 export default function Index() {
   return (
     <View style={styles.container}>
       {/* <HomeScreen/> */}
-      <RestaurantDetailsPage/>
+      {/* <RestaurantDetailsPage/> */}
+      <DishDetailsScreen/>
     </View>
   );
 }
@@ -14,7 +16,7 @@ export default function Index() {
 const styles = StyleSheet.create({
   container: {
     flex:1,
-    backgroundColor: "lightblue",
+    backgroundColor: "lightblue", 
     alignItems:"center",
     justifyContent:"center",
     padding:10,
---
+++ my-app/app/screens/DishDetailsScreen/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import {View, Text, StyleSheet} from 'react-native';
+
+const DishDetailsScreen = () => {
+    return (
+        <View>
+            <Text>Dish Details</Text>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({});
+
+export default DishDetailsScreen
