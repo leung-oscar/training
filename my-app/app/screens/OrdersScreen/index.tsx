94ae971e111ed350d47f3ca59b09bb5a7dec3cab
leung-oscar <leung.oscar@dabba-eats.com>
2025-01-15 00:20:38 +0000

    Add flatlist to orderr list item

--- my-app/app/screens/OrdersScreen/index.tsx
+++ my-app/app/screens/OrdersScreen/index.tsx
@@ -1,14 +1,17 @@
 import React from "react"
 import {View, Text, FlatList} from "react-native"
-import OrderListItems from "my-app/assets/components/OrderListItem.jsx"
+import OrderListItems from "/workspaces/training/my-app/assets/components/orderListItem.jsx"
 import orders from "/workspaces/training/my-app/assets/data/orders.json"
+
+const order = orders[0]
 
 const OrderScreen = () => {
     return (
         <View>
-            <Text>
-                OrderScreen
-            </Text>
+            <FlatList
+                data={orders}
+                renderItem={({item}) => <OrderListItems order={item}/>}
+            />
         </View>
     )
 }
