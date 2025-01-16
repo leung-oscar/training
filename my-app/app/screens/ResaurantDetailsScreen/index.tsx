6e49cd054ab29d43393ae8905b245a2466e186ec
leung-oscar <leung.oscar@dabba-eats.com>
2025-01-16 21:29:43 +0000

    Implement goback

--- my-app/app/screens/ResaurantDetailsScreen/index.tsx
+++ my-app/app/screens/ResaurantDetailsScreen/index.tsx
@@ -6,13 +6,16 @@ import DishListItem from '/workspaces/training/my-app/assets/components/dishList
 import Header from '/workspaces/training/my-app/app/screens/ResaurantDetailsScreen/header.tsx'
 import styles from '/workspaces/training/my-app/app/screens/ResaurantDetailsScreen/styles.tsx';
 
-import { useRoute } from '@react-navigation/native';
+import { useRoute, useNavigation } from '@react-navigation/native';
 
 const restaurant = restaurants[0];
 
 
 const RestaurantDetailsPage = () => {
+    
     const route = useRoute();
+    const navigation = useNavigation();
+
     const id = route.params?.id;
     console.warn(id);
 
@@ -21,6 +24,7 @@ const RestaurantDetailsPage = () => {
     return (
         <View style={styles.page}>
             <Ionicons
+                onPress={navigation.goBack}
                 name='arrow-back-circle'
                 size={45}
                 color="white"
